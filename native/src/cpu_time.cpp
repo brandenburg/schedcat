6e7c15480d7f5e57636d5648c97c4277eea2c704
Bjoern Brandenburg <bbb@mpi-sws.org>
2012-08-06 18:27:32 +0200

    Add support for clock_gettime
    Use the more accurate clock under Linux when available. getrusage()
    unfortunately only has a resolution of 1/HZ (i.e., one jiffie), which
    is too coarse-grained in many cases.

--- native/src/cpu_time.cpp
+++ native/src/cpu_time.cpp
@@ -1,8 +1,31 @@
 
+#include <time.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 
 #include "cpu_time.h"
+
+
+#if _POSIX_C_SOURCE >= 199309L
+
+// use clock_xxx() API
+
+double get_cpu_usage(void)
+{
+	struct timespec ts;
+
+	if (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts) == 0)
+	{
+		return ts.tv_sec + ts.tv_nsec / 1E9;
+	}
+	else
+		return 0.0;
+}
+
+
+#else
+
+// fall back to getrusage()
 
 #ifdef RUSAGE_THREAD
 // This is a Linuxism...
@@ -23,3 +46,4 @@ double get_cpu_usage(void)
 		return 0.0;
 }
 
+#endif
