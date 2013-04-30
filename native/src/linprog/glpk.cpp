ad4c98170104aa820ba6eec614a40e2a6b0340b8
Alexander Wieder <awieder@mpi-sws.org>
2013-04-30 15:34:18 +0200

    Add ILP support to GLPK bridge
    In the testmain MSRP case, the produces the same numbers as CPLEX.

--- native/src/linprog/glpk.cpp
+++ native/src/linprog/glpk.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <glpk.h>
+#include <stdlib.h>
 
 #include <iostream>
 
@@ -15,6 +16,7 @@ private:
 	const unsigned int num_cols;
 	const unsigned int num_rows;
 	unsigned int num_coeffs;
+	const bool is_mip;
 
 	bool solved;
 
@@ -22,6 +24,7 @@ private:
 	void set_objective();
 	void set_bounds(double col_lb, double col_ub);
 	void set_coefficients();
+	void set_column_types();
 public:
 
 	GLPKSolution(const LinearProgram &lp, unsigned int max_num_vars,
@@ -31,7 +34,10 @@ public:
 
 	double get_value(unsigned int var) const
 	{
-		return glp_get_col_prim(glpk, var + 1);
+		if (is_mip)
+			return glp_mip_col_val(glpk, var + 1);
+		else
+			return glp_get_col_prim(glpk, var + 1);
 	}
 
 	bool is_solved() const
@@ -48,6 +54,7 @@ GLPKSolution::GLPKSolution(const LinearProgram& lp, unsigned int max_num_vars,
 	  num_rows(lp.get_equalities().size() +
 		   lp.get_inequalities().size()),
 	  num_coeffs(0),
+	  is_mip(lp.has_binary_variables() || lp.has_integer_variables()),
 	  solved(false)
 {
 	if (num_cols)
@@ -81,27 +88,46 @@ void GLPKSolution::solve(double var_lb, double var_ub)
 	set_objective();
 	set_bounds(var_lb, var_ub);
 	set_coefficients();
+	if (is_mip)
+		set_column_types();
 
 #if DEBUG_LP_OVERHEADS >= 3
 	model_costs.stop();
 	solver_costs.start();
 #endif
 
-	glp_smcp glpk_params;
+	if (is_mip)
+	{
+		glp_iocp glpk_params;
 
-	glp_init_smcp(&glpk_params);
+		glp_init_iocp(&glpk_params);
 
-	/* Set solver options. The presolver is essential. The other two
-	 * options seem to make the solver slightly faster.
-	 *
-	 * Tested with GLPK 4.43 on wks-50-12.
-	 */
-	glpk_params.presolve = GLP_ON;
-	glpk_params.pricing  = GLP_PT_STD;
-	glpk_params.r_test   = GLP_RT_STD;
+		// presolver is required because otherwise
+		// GLPK expects glpk to hold an optimal solution
+		// to the relaxed LP.
+		glpk_params.presolve = GLP_ON;
 
-	solved = glp_simplex(glpk, &glpk_params) == 0 &&
-		glp_get_status(glpk) == GLP_OPT;
+		solved = glp_intopt(glpk, &glpk_params) == 0 &&
+			 glp_mip_status(glpk) == GLP_OPT;
+	}
+	else
+	{
+		glp_smcp glpk_params;
+
+		glp_init_smcp(&glpk_params);
+
+		/* Set solver options. The presolver is essential. The other two
+		 * options seem to make the solver slightly faster.
+		 *
+		 * Tested with GLPK 4.43 on wks-50-12.
+		 */
+		glpk_params.presolve = GLP_ON;
+		glpk_params.pricing  = GLP_PT_STD;
+		glpk_params.r_test   = GLP_RT_STD;
+
+		solved = glp_simplex(glpk, &glpk_params) == 0 &&
+			glp_get_status(glpk) == GLP_OPT;
+	}
 
 #if DEBUG_LP_OVERHEADS >= 3
 	solver_costs.stop();
@@ -199,6 +225,24 @@ void GLPKSolution::set_coefficients()
 	delete[] coeff;
 }
 
+void GLPKSolution::set_column_types()
+{
+	unsigned int col_idx;
+
+	foreach(linprog.get_integer_variables(), var_id)
+	{
+		col_idx = 1 + *var_id;
+		// hack: for integer variables, ignore upper bound for now
+		glp_set_col_bnds(glpk, col_idx, GLP_LO, 0, 0);
+		glp_set_col_kind(glpk, col_idx, GLP_IV);
+	}
+
+	foreach(linprog.get_binary_variables(), var_id)
+	{
+		col_idx = 1 + *var_id;
+		glp_set_col_kind(glpk, col_idx, GLP_BV);
+	}
+}
 
 Solution *glpk_solve(const LinearProgram& lp, unsigned int max_num_vars)
 {
