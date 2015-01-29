479b3157c81a383905ad33db6d6b2df363f7c929
Björn Brandenburg <bbb@mpi-sws.org>
2015-01-29 14:58:14 +0100

    C++: add pretty-printers for STL sets and vectors
    Simplifies debugging...

---
+++ native/include/stl-io-helper.h
@@ -0,0 +1,102 @@
+#ifndef STL_IO_HELPER_H
+#define STL_IO_HELPER_H
+
+#include <iostream>
+#include <set>
+#include <vector>
+#include <map>
+
+#include "stl-hashmap.h"
+#include "stl-helper.h"
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::set<T> &s)
+{
+	bool first = true;
+	os << "{";
+	foreach(s, e)
+	{
+		if (!first)
+			os << ", ";
+		os << *e;
+		first = false;
+	}
+	os << "}";
+
+	return os;
+}
+
+template <typename K, typename V>
+std::ostream& operator<<(std::ostream &os, const std::map<K,V> &s)
+{
+	bool first_elem = true;
+	os << "{";
+	foreach(s, e)
+	{
+		if (!first_elem)
+			os << ", ";
+		os << e->first;
+		os << ": ";
+		os << e->second;
+		first_elem = false;
+	}
+	os << "}";
+
+	return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const hashset<T> &s)
+{
+	bool first = true;
+	os << "{";
+	foreach(s, e)
+	{
+		if (!first)
+			os << ", ";
+		os << *e;
+		first = false;
+	}
+	os << "}";
+
+	return os;
+}
+
+template <typename K, typename V>
+std::ostream& operator<<(std::ostream &os, const hashmap<K,V> &s)
+{
+	bool first_elem = true;
+	os << "{";
+	foreach(s, e)
+	{
+		if (!first_elem)
+			os << ", ";
+		os << e->first;
+		os << ": ";
+		os << e->second;
+		first_elem = false;
+	}
+	os << "}";
+
+	return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &s)
+{
+	bool first = true;
+	os << "[";
+	foreach(s, e)
+	{
+		if (!first)
+			os << ", ";
+		os << *e;
+		first = false;
+	}
+	os << "]";
+
+	return os;
+}
+
+
+#endif
