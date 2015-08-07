a11fb0caf4f05d4c81092842d146d2c95a25e9f2
Björn Brandenburg <bbb@mpi-sws.org>
2015-08-07 22:58:03 +0200

    C++: Add global analysis for priority-ordered locks w/o progress mechanism
    Based on an initial implementation by Maolin Yang and Alexander Wieder.

--- native/src/blocking/linprog/lp_no_progress_priority.cpp
+++ native/src/blocking/linprog/lp_no_progress_priority.cpp
@@ -18,13 +18,35 @@
 #include "res_io.h"
 #include "linprog/io.h"
 
+#include "lp_global.h"
+
+
+class GlobalPrioNoProgressAnalysis
+	: public GlobalNoProgressMechanismLP, public GlobalPriorityQueuesLP
+{
+
+public:
+	GlobalPrioNoProgressAnalysis(const ResourceSharingInfo& info,
+					unsigned int i,
+					unsigned int number_of_cpus)
+		: GlobalSuspensionAwareLP(info, i, number_of_cpus),
+		  GlobalNoProgressMechanismLP(info, i, number_of_cpus),
+		  GlobalPriorityQueuesLP(info, i, number_of_cpus)
+	{
+	}
+};
+
 BlockingBounds* lp_no_progress_priority_bounds(
 	const ResourceSharingInfo& info,
 	unsigned int number_of_cpus)
 {
 	BlockingBounds* results = new BlockingBounds(info);
 
-	NYI();
+	for (unsigned int i = 0; i < info.get_tasks().size(); i++)
+	{
+		GlobalPrioNoProgressAnalysis lp(info, i, number_of_cpus);
+		(*results)[i] = lp.solve();
+	}
 
 	return results;
 }
