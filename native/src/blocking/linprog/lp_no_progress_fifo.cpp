814403dd5998c8f553433e7485062ace91e41c91
Björn Brandenburg <bbb@mpi-sws.org>
2015-08-07 22:56:28 +0200

    C++: Add global analysis for FIFO locks w/o progress mechanism
    Based on an initial implementation by Maolin Yang and Alexander Wieder.

--- native/src/blocking/linprog/lp_no_progress_fifo.cpp
+++ native/src/blocking/linprog/lp_no_progress_fifo.cpp
@@ -18,13 +18,34 @@
 #include "res_io.h"
 #include "linprog/io.h"
 
+#include "lp_global.h"
+
+class GlobalFIFONoProgressAnalysis
+	: public GlobalNoProgressMechanismLP, public GlobalFIFOQueuesLP
+{
+
+public:
+	GlobalFIFONoProgressAnalysis(const ResourceSharingInfo& info,
+					unsigned int i,
+					unsigned int number_of_cpus)
+		: GlobalSuspensionAwareLP(info, i, number_of_cpus),
+		  GlobalNoProgressMechanismLP(info, i, number_of_cpus),
+		  GlobalFIFOQueuesLP(info, i, number_of_cpus)
+	{
+	}
+};
+
 BlockingBounds* lp_no_progress_fifo_bounds(
 	const ResourceSharingInfo& info,
 	unsigned int number_of_cpus)
 {
 	BlockingBounds* results = new BlockingBounds(info);
 
-	NYI();
+	for (unsigned int i = 0; i < info.get_tasks().size(); i++)
+	{
+		GlobalFIFONoProgressAnalysis lp(info, i, number_of_cpus);
+		(*results)[i] = lp.solve();
+	}
 
 	return results;
 }
