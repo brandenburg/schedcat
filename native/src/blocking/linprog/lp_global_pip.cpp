d2fdbd4b0ed096bb29194d711d2df97799acf2b5
Björn Brandenburg <bbb@mpi-sws.org>
2015-08-07 22:33:26 +0200

    C++: Add global PIP analysis
    Based on an initial implementation by Maolin Yang and Alexander Wieder.

--- native/src/blocking/linprog/lp_global_pip.cpp
+++ native/src/blocking/linprog/lp_global_pip.cpp
@@ -18,6 +18,29 @@
 #include "res_io.h"
 #include "linprog/io.h"
 
+#include "lp_global.h"
+
+
+class GlobalPIPAnalysis : public GlobalPrioInheritanceLP, public GlobalPriorityQueuesLP
+{
+
+public:
+	GlobalPIPAnalysis(const ResourceSharingInfo& info,
+					unsigned int i,
+					unsigned int number_of_cpus)
+		: GlobalSuspensionAwareLP(info, i, number_of_cpus),
+		  GlobalPrioInheritanceLP(info, i, number_of_cpus),
+		  GlobalPriorityQueuesLP(info, i, number_of_cpus)
+	{
+		// Protocol-specific constraints
+
+		// Constraint 11
+		add_pip_fmlp_no_stalling_interference();
+		// Constraint 12
+		add_pip_ppcp_indirect_preemption_constraints();
+	}
+};
+
 
 BlockingBounds* lp_global_pip_bounds(
 	const ResourceSharingInfo& info,
@@ -25,7 +48,11 @@ BlockingBounds* lp_global_pip_bounds(
 {
 	BlockingBounds* results = new BlockingBounds(info);
 
-	NYI();
+	for (unsigned int i = 0; i < info.get_tasks().size(); i++)
+	{
+		GlobalPIPAnalysis lp(info, i, number_of_cpus);
+		(*results)[i] = lp.solve();
+	}
 
 	return results;
 }
