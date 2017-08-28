a741f181d7aa50a58b4850c5d6601fbeeecfdf5e
Björn Brandenburg <bbb@mpi-sws.org>
2017-08-29 00:24:15 +0200

    Stop relying on ext::hash_map<>

--- native/include/stl-hashmap.h
+++ native/include/stl-hashmap.h
@@ -1,39 +1,9 @@
 #ifndef STL_HASHMAP_H_
 #define STL_HASHMAP_H_
 
-#ifdef CONFIG_USE_0X
 #include <unordered_map>
 #include <unordered_set>
 #define hashmap std::unordered_map
 #define hashset std::unordered_set
-#else
-#include <ext/hash_map>
-#include <ext/hash_set>
-
-namespace __gnu_cxx
-{
-template<>
-struct hash<long long int>
-{
-	size_t operator()(long long int __x) const
-	{
-		return __x;
-	}
-};
-
-template<>
-struct hash<unsigned long long int>
-{
-	size_t operator()(unsigned long long int __x) const
-	{
-		return __x;
-	}
-};
-
-};
-
-#define hashmap __gnu_cxx::hash_map
-#define hashset __gnu_cxx::hash_set
-#endif
 
 #endif
